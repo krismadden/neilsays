204a61cfe351aa6f6085d19ca81149fbe4bf6751
krismadden <madden_kris@yahoo.com>
2017-12-09 16:57:33 +0100

    sync wih mac.

--- noscenes5/ledtest.py
+++ noscenes5/ledtest.py
@@ -17,6 +17,35 @@ GPIO.setup(18,GPIO.OUT)
 GPIO.setup(15,GPIO.OUT)
 GPIO.setup(14,GPIO.OUT)
 
+def LEDoff():
+	GPIO.setup(21,GPIO.LOW)
+	GPIO.setup(20,GPIO.LOW)
+	GPIO.setup(16,GPIO.LOW)
+	GPIO.setup(12,GPIO.LOW)
+	GPIO.setup(7,GPIO.LOW)
+	GPIO.setup(8,GPIO.LOW)
+	GPIO.setup(25,GPIO.LOW)
+	GPIO.setup(24,GPIO.LOW)
+	GPIO.setup(23,GPIO.LOW)
+	GPIO.setup(18,GPIO.LOW)
+	GPIO.setup(15,GPIO.LOW)
+	GPIO.setup(14,GPIO.LOW)
+
+def LEDon():
+	GPIO.setup(21,GPIO.HIGH)
+	GPIO.setup(20,GPIO.HIGH)
+	GPIO.setup(16,GPIO.HIGH)
+	GPIO.setup(12,GPIO.HIGH)
+	GPIO.setup(7,GPIO.HIGH)
+	GPIO.setup(8,GPIO.HIGH)
+	GPIO.setup(25,GPIO.HIGH)
+	GPIO.setup(24,GPIO.HIGH)
+	GPIO.setup(23,GPIO.HIGH)
+	GPIO.setup(18,GPIO.HIGH)
+	GPIO.setup(15,GPIO.HIGH)
+	GPIO.setup(14,GPIO.HIGH)
+	
+
 #print "LED on"
 LEDon()
 time.sleep(1)
@@ -55,30 +84,3 @@ LEDoff()
 
 
 
-def LEDoff():
-	GPIO.setup(21,GPIO.LOW)
-	GPIO.setup(20,GPIO.LOW)
-	GPIO.setup(16,GPIO.LOW)
-	GPIO.setup(12,GPIO.LOW)
-	GPIO.setup(7,GPIO.LOW)
-	GPIO.setup(8,GPIO.LOW)
-	GPIO.setup(25,GPIO.LOW)
-	GPIO.setup(24,GPIO.LOW)
-	GPIO.setup(23,GPIO.LOW)
-	GPIO.setup(18,GPIO.LOW)
-	GPIO.setup(15,GPIO.LOW)
-	GPIO.setup(14,GPIO.LOW)
-
-def LEDon():
-	GPIO.setup(21,GPIO.HIGH)
-	GPIO.setup(20,GPIO.HIGH)
-	GPIO.setup(16,GPIO.HIGH)
-	GPIO.setup(12,GPIO.HIGH)
-	GPIO.setup(7,GPIO.HIGH)
-	GPIO.setup(8,GPIO.HIGH)
-	GPIO.setup(25,GPIO.HIGH)
-	GPIO.setup(24,GPIO.HIGH)
-	GPIO.setup(23,GPIO.HIGH)
-	GPIO.setup(18,GPIO.HIGH)
-	GPIO.setup(15,GPIO.HIGH)
-	GPIO.setup(14,GPIO.HIGH)
